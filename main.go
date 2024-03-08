32dc30865191689277ac4e0f57ce823dae4762de
Anthony Lockett <alockettjr@me.com>
2024-03-07 23:22:48 -0500

    added git collections

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import(
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"net/http"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
 )
 
 func RenderIndex(c *fiber.Ctx) error {
 	return c.Render("index", fiber.Map{})
+}
+
+func RenderCollections(c *fiber.Ctx) error {
+	service := os.Getenv("COLLECTION_SERVICE_DOMAIN")
+	response, err := http.Get(service)
+	if err != nil {
+		fmt.Println("Error in calling Colleciton domain")
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c.Send(responseData)
 }
 
 func main() {
@@ -19,6 +37,7 @@ func main() {
 	app.Static("/", "./static")
 
 	app.Get("/", RenderIndex)
+	app.Get("/collections", RenderCollections)
 
 	port := os.Getenv("PORT")
 
